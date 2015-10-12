56b7adad0e6f9b6350fa7aff8859b3a73c778003
babypaunch <babypaunch@gmail.com>
2015-10-13 00:58:22 +0900

    add sample
    1. pagify 수정
    2. index.html 추가

--- jquery.babypaunch.pagify.js
+++ jquery.babypaunch.pagify.js
@@ -10,18 +10,19 @@ $.fn.pagify = function(settings){
     var defaults = {
         current: 1 //현재 페이지 번호(text)
         , total: 0 //전체 건수
-        , pager: 5 //web에서는 보통 pager를 10개로 노출한다.
+        , pager: 10 //web에서는 보통 pager를 10개로 노출한다.
         , combo: [10, 20, 30, 50] //콤보박스의 선택에 대한 부분은 추후 개발 예정임.
-        , list: 10 //한 화면에 listing될 갯수를 지정.
-        , showCombo: false //콤보박스의 선택에 대한 부분은 추후 개발 예정임.
-        , showFirstEnd: //처음/끝 버튼을 보일지 여부.
+        , list: 20 //한 화면에 listing될 갯수를 지정.
+        , showCombo: true//콤보박스의 선택에 대한 부분은 추후 개발 예정임.
+        , showFirstEnd: true//처음/끝 버튼을 보일지 여부.
+		, attr: "offset" //set custom attr name
         
         , first: function(min){ //'처음' 버튼
-            return {"offset": min - 1, "text": min};
+            return {[this.attr] : min - 1, "text": min};
         }
         , prev: function(num, min){ //'이전' 버튼
             var _prev = (num - 1) * this.pager - this.pager + 1;
-            return _prev < min ? {"offset": min - 1, "text": min} : {"offset": _prev - 1, "text": _prev};
+            return _prev < min ? {[this.attr]: min - 1, "text": min} : {[this.attr]: _prev - 1, "text": _prev};
         }
         , from: function(num){ //'pager 시작' 번호
             return num === 1 ? 1 : num * this.pager - this.pager + 1;
@@ -31,10 +32,10 @@ $.fn.pagify = function(settings){
         }
         , next: function(num, max){ //'다음' 버튼
             var _next = num * this.pager + 1;
-            return _next > max ? {"offset": max - 1, "text": max} : {"offset": _next - 1, "text": _next};
+            return _next > max ? {[this.attr]: max - 1, "text": max} : {[this.attr]: _next - 1, "text": _next};
         }
         , end: function(max){ //'끝' 버튼
-            return {"offset": max - 1, "text": max};
+            return {[this.attr]: max - 1, "text": max};
         }
         
         /*
@@ -43,21 +44,57 @@ $.fn.pagify = function(settings){
         * 동적으로 그려진 객체가 되며, 클릭에 대한 이벤트는 플러그인 외부에서 처리하면 됨.
         */
         , ui: {
-            first: function(offset, text){ //'처음' 버튼
-                return "<a href='#' data-offset='" + offset + "'> << </a>";
+			style: function(idx){
+				var _style = "text-decoration: none; font-size: 1.2em; pointer: cursor;";
+				switch(idx){
+					case 0:
+						_style += " color: silver; padding: 0.3em;"
+					break;
+					case 1:
+						_style += " color: gray; padding: 0.3em; font-weight: bold;"
+					break;
+					default:
+						_style += " color: gray; font-weight: bold;"
+					break;
+				}
+				return " style='" + _style + "'";
+			}
+			/* easy set buttons
+            , first: function(num, text){ //'처음' 버튼
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style() + "> ≪ </a>\n";
             }
-            , prev: function(offset, text){ //'이전' 버튼
-                return "<a href='#' data-offset='" + offset + "'> < </a>";
+            , prev: function(num, text){ //'이전' 버튼
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style() + "> ＜ </a>\n";
             }
-            , pager: function(offset, text, isCurrent){ //'pager' 버튼 전체
-                return "<a href='#' data-offset='" + offset + "'" + (isCurrent ? " style='font-weight: bold;'" : "") + ">" + text + "</a>";
+            , pager: function(num, text, isCurrent){ //'pager' 버튼 전체
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style(isCurrent ? 1 : 0) + ">" + text + "</a>\n";
             }
-            , next: function(offset, text){ //'다음' 버튼
-                return "<a href='#' data-offset='" + offset + "'> > </a>";
+            , next: function(num, text){ //'다음' 버튼
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style() + "> ＞ </a>\n";
             }
-            , end: function(offset, text){ //'끝' 버튼
-                return "<a href='#' data-offset='" + offset + "'> >> </a>";
+            , end: function(num, text){ //'끝' 버튼
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style() + "> ≫ </a>\n";
             }
+			*/
+			/* default set buttons */
+			, nav: function(type, num, text){
+				var _type = "";
+				switch(type){
+					case "first":
+						_type = "≪";
+					break;
+					case "prev":
+						_type = "＜";
+					break;
+					case "next":
+						_type = "＞";
+					break;
+					case "end":
+						_type = "≫";
+					break;
+				}
+                return "<a href='#' data-" + defaults.attr + "='" + num + "'" + this.style() + "> " + _type + " </a>\n";
+			}
         } //end: , ui: {
     } //end: var defaults = {
     
@@ -75,14 +112,14 @@ $.fn.pagify = function(settings){
         var inPaged = false;
         
         /* pager fromTo를 먼저 구한다. */
-        for(var j = defaults.from(i), offset = j - 1; j <= defaults.to(i); j++, offset++){ 
+        for(var j = defaults.from(i), num = j - 1; j <= defaults.to(i); j++, num++){ 
             if(j <= listed){
                 if(j === defaults.current){
                     inPaged = true;
                 }
-                fromTo.push({"offset": offset, "text": j, "isCurrent": j === defaults.current});
+                fromTo.push({[defaults.attr]: num, "text": j, "isCurrent": j === defaults.current});
             }
-        } //end: for(var j = defaults.from(i), offset = j - 1; j <= defaults.to(i); j++, offset++){
+        } //end: for(var j = defaults.from(i), num = j - 1; j <= defaults.to(i); j++, num++){
         
         if(inPaged){ //보여줄 pager에 해당되면
             if(defaults.showFirstEnd && paged > 1 && defaults.current !== 1){ //'처음' 버튼 추가 여부
@@ -111,18 +148,25 @@ $.fn.pagify = function(settings){
         */
         for(var i in result){
             var _result = result[i];
-            if(i === "prev"){
-                tags += _ui.prev(_result.offset, _result.text);
-            }
             if(i === "fromTo"){
                 for(var j = 0; j < _result.length; j++){
-                    tags += _ui.pager(_result[j].offset, _result[j].text, _result[j].isCurrent);
+                    tags += _ui.pager(_result[j][defaults.attr], _result[j].text, _result[j].isCurrent);
                 }
-            }
-            if(i === "next"){
-                tags += _ui.next(_result.offset, _result.text);
-            }
+            }else{
+                //tags += _ui[i](_result[defaults.attr], _result.text); //easy set buttons
+                tags += _ui.nav(i, _result[defaults.attr], _result.text); //default set buttons
+			}
         } //end: for(var i in result){
+
+		if(defaults.showCombo){
+			tags += "<select style='font-size: 1em;'>\n";
+			for(var i = 0, combo = defaults.combo; i < combo.length; i++){
+				tags += "\t<option value='" + combo[i] + "'" + (defaults.list === combo[i] ? " selected='selected'" : "") + ">"
+					+ combo[i]
+				+ "</option>\n";
+			}
+			tags += "</select>\n";
+		} //end: if(defaults.showCombo){
         
         $(this).html(tags); //해당 객체에 결과 그려넣기.
     }); //end: return this.each(function(){
